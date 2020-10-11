c06ee24ce8dadb4a951068f4eb489540e197a05e
marnysan111 <o-masato@outlook.jp>
2020-10-11 21:47:22 +0900

    10/11

--- kadai1/marny/sample/sample.go
+++ kadai1/marny/sample/sample.go
@@ -1,12 +1,26 @@
-// Copyright
-// +build darwin
+/*
+ここはパッケージコメントの最初になるから見出しではないよー
 
+Hで始まり単一行かつ句読点なしかつ前が見出しではないのでこれは見出し
+
+段落段落
+段落段落
+段落段落
+
+次の段落
+次の段落
+
+    整形済みテキスt
+
+次のやつはリンクになるはず。
+https://golang.org/
+*/
 package sample
 
 func Hoge(str string) string {
 	str2 := str + "HAHAHA"
-
 	return str2
+	// Output: aaa
 }
 
 //Article は1つの記事を表します。
@@ -16,4 +30,8 @@ type Article struct {
 
 	// 記事本文
 	Body string
+}
+
+type Test struct {
+	A string
 }
